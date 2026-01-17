435047b22c3b4558ce6a9032ef888a6d3b295e71
Viktoriia Melnychuk <s34641@pjwstk.edu.pl>
2026-01-17 10:45:27 +0100

    Initial Spring Boot project

---
+++ src/main/java/pl/edu/pwr/s34641bank/S34641bankApplication.java
@@ -0,0 +1,13 @@
+package pl.edu.pwr.s34641bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S34641bankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S34641bankApplication.class, args);
+	}
+
+}
